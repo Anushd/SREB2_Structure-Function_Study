36ec4eadadbdd5ae14ef0763dbfebf3280d5560f
Anush Devv <devadhasan.a@husky.neu.edu>
2017-11-24 20:17:45 -0500

    Delete script_3.py

--- script_3.py
+++
@@ -1,9 +0,0 @@
-from modeller import *
-from modeller.automodel import *
-
-env = environ()
-a = automodel(env, alnfile='qseq1-mult.ali',
-              knowns=('tseq1A','tseq2A','tseq3A'), sequence='qseq1')
-a.starting_model = 1
-a.ending_model = 5
-a.make()
