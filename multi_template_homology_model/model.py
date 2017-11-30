63756ba1b3126f30fc2ea85ddf6094331814cfcc
Anush Devv <devadhasan.a@husky.neu.edu>
2017-11-29 21:43:39 -0500

    Add files via upload

---
+++ multi_template_homology_model/model.py
@@ -0,0 +1,11 @@
+from modeller import *
+from modeller.automodel import *
+
+env = environ()
+a = automodel(env, alnfile='align-mult.ali',
+              knowns=('5u09A','5uenA','1u19A'), sequence='qseq1',assess_methods=(assess.DOPE,
+                              #soap_protein_od.Scorer(),
+                              assess.GA341))
+a.starting_model = 1
+a.ending_model = 5
+a.make()
